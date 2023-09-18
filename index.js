f47a7c1c3298841274de571a81026368550064e6
accenggg <arseliusngiklan@gmail.com>
2023-09-18 22:58:42 +0800

    initialize commit

--- index.js
+++ index.js
@@ -38,30 +38,73 @@ const url = require ('url')
 
 ////////////////////////////////////
 // SERVER dengan HTTP
-const server = http.createServer((req, res) => {
-    const pathName = req.url
 
+const replaceTemplate = (template,product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+    return output
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
+
+const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
+const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+
+const server = http.createServer((req, res) => {
+    // const pathName = req.url
+    const { pathname : pathName, query } = url.parse(req.url, true)
+
+    // HELLO PAGE
     if (pathName === '/hello'){
         res.end('ini hello ke FSW 2')
-    } else if (pathName === '/product'){
-        res.end(JSON.stringify({
-            data: 'Ini product'
-        }));
-    } else if(pathName === '/api'){
-        const data = fs.readFileSync(`${__dirname}/dev-data/data.json`)
+    } 
+    
+    // PRODUCT PAGE
+    else if (pathName === '/product'){
+        console.log(query);
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        })
+        const product = dataObj[query.id]
+        const output = replaceTemplate(productTemplate, product)
+        res.end(output)
+    } 
+    
+    // SIMPLE API
+    else if(pathName === '/api'){
         res.writeHead(200, {
             'Content-Type' : 'application/json'
         })
 
         res.end(data)
     } 
+    
+    // OVERVIEW PAGE
     else if (pathName === '/overview'){
-        const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`)
         res.writeHead(200, {
             'Content-Type' : 'text/html'
         })
-        res.end(overviewPage)
+        
+        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el))
+
+        const output = overviewPage.replace('{%PRODUCTCARDS%}', productCardsHtml)
+        res.end(output)
     }
+    
+    // 
     else {
         res.writeHead(404, {
             'Content-Type' : 'text/html'
