01bda3838c455790f60c4e141f7a13975f4f31b6
accenggg <arseliusngiklan@gmail.com>
2023-12-08 12:20:32 +0800

    setting vercel to access overview.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const http = require ('http')
-const url = require ('url')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 ////////////////////////////////////
 // FILES dengan FS
@@ -13,7 +13,6 @@ const url = require ('url')
 // fs.writeFileSync('./txt/output-penjelasan.txt', textOut)
 // console.log('sukses nyetak surat cinta!');
 
-
 // non blocking code execution -> asynchronous
 // const test = fs.readFile('./txt/start.txt', 'utf-8', (err, data)=>{
 //     if(err) return console.log('Ini error');
@@ -25,12 +24,12 @@ const url = require ('url')
 //         })
 
 //         fs.readFile(`./txt/final.txt`, 'utf-8',(err,data3) => {
-        
+
 //             fs.writeFile(`./txt/gabungan2.txt`, `${data2}\n${data3}`, err  => {
 //                 console.log('sukses menggabungkan data2');
 //             })
 //         })
-        
+
 //     })
 
 // })
@@ -39,82 +38,91 @@ const url = require ('url')
 ////////////////////////////////////
 // SERVER dengan HTTP
 
-const replaceTemplate = (template,product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
 
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%ID%}/g, product.id)
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
 
-    return output
-}
+  return output;
+};
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
-
-const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
-const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
-const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const overviewPage = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+const productCardTemplate = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 
 const server = http.createServer((req, res) => {
-    // const pathName = req.url
-    const { pathname : pathName, query } = url.parse(req.url, true)
+  // const pathName = req.url
+  const { pathname: pathName, query } = url.parse(req.url, true);
 
-    // HELLO PAGE
-    if (pathName === '/hello'){
-        res.end('ini hello ke FSW 2')
-    } 
-    
-    // PRODUCT PAGE
-    else if (pathName === '/product'){
-        console.log(query);
-        res.writeHead(200, {
-            'Content-Type' : 'text/html'
-        })
-        const product = dataObj[query.id]
-        const output = replaceTemplate(productTemplate, product)
-        res.end(output)
-    } 
-    
-    // SIMPLE API
-    else if(pathName === '/api'){
-        res.writeHead(200, {
-            'Content-Type' : 'application/json'
-        })
+  // HELLO PAGE
+  if (pathName === "/hello") {
+    res.end("ini hello ke FSW 2");
+  }
 
-        res.end(data)
-    } 
-    
-    // OVERVIEW PAGE
-    else if (pathName === '/overview'){
-        res.writeHead(200, {
-            'Content-Type' : 'text/html'
-        })
-        
-        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el))
+  // PRODUCT PAGE
+  else if (pathName === "/product") {
+    console.log(query);
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(productTemplate, product);
+    res.end(output);
+  }
 
-        const output = overviewPage.replace('{%PRODUCTCARDS%}', productCardsHtml)
-        res.end(output)
-    }
-    
-    // 
-    else {
-        res.writeHead(404, {
-            'Content-Type' : 'text/html'
-        })
-        res.end('<h1>url kosong</h1>')
-    }
+  // SIMPLE API
+  else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
 
-});
-  
-server.listen(8000,'127.0.0.1', ()=> {
-    console.log('Server berjalan di http://localhost:8000');
+    res.end(data);
+  }
+
+  // OVERVIEW PAGE
+  else if (pathName === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const productCardsHtml = dataObj.map((el) =>
+      replaceTemplate(productCardTemplate, el)
+    );
+
+    const output = overviewPage.replace("{%PRODUCTCARDS%}", productCardsHtml);
+    res.end(output);
+  }
+
+  //
+  else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>url kosong</h1>");
+  }
 });
 
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server berjalan di http://localhost:8000");
+});
